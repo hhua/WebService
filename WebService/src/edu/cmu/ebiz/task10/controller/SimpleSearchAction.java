592f03b8d66be92d68ccb7288585d749ea25c04e
chinesecold <chinesecold@gmail.com>
2013-02-14 13:57:40 -0500

    Google PlaceS API test added

---
+++ WebService/src/edu/cmu/ebiz/task10/controller/SimpleSearchAction.java
@@ -0,0 +1,19 @@
+package edu.cmu.ebiz.task10.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class SimpleSearchAction extends Action {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "simple-search.do";
+	}
+
+	@Override
+	public String perform(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
