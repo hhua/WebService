8727b0c35ab781958a814cdda6ace9c99de3421d
tzheng <tzheng@andrew.cmu.edu>
2013-02-14 13:39:43 -0500

    add controller, action, model

--- WebService/src/edu/cmu/ebiz/task10/test/Zillow.java
+++ WebService/src/edu/cmu/ebiz/task10/test/Zillow.java
@@ -29,7 +29,10 @@ public class Zillow {
 	public static void main(String[] args) {
 		/**
 		 * http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1di5yjoznd7_2ca3z&address=5662+Beacon+Street&citystatezip=Pittsburgh%2C+PA%22
-		 *	the link above can get price of a house
+		 *	the link above can get price of a house, but 
+		 *
+		 *demographic:
+		 *http://www.fizber.com/services/fizber_xml.html#nei_city
 		 *
 		 *get neighborhood  info
 		 *http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1di5yjoznd7_2ca3z&state=PA&city=Pittsburgh&neighborhood=North%20Oakland
@@ -40,7 +43,7 @@ public class Zillow {
 			 */
 			String state = "PA";
 			String city = "Pittsburgh";
-			String neighbor = "North Oakland";
+			String neighbor = "North%20Oakland";
 			//http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1di5yjoznd7_2ca3z&state=PA&city=Pittsburgh&neighborhood=North%20Oakland
 			String preURL = "http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1di5yjoznd7_2ca3z&state="+state
 					+"&city=" + city 
@@ -83,7 +86,7 @@ public class Zillow {
     			 * we can also get other information
     			 * 
     			 */
-    			XPathExpression expr = xpath.compile("//chart[name='Median Condo Value']/name/text()");
+    			XPathExpression expr = xpath.compile("//region/*/text()");
     			
     			Object result = expr.evaluate(doc, XPathConstants.NODESET);
     			NodeList nodes = (NodeList) result;
---
+++ WebService/src/edu/cmu/ebiz/task8/model/Model.java
@@ -0,0 +1,19 @@
+/**
+ * @author Team Snipers (Team 1)
+ * Jan 17, 2013
+ */
+
+package edu.cmu.ebiz.task8.model;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+public class Model {
+	
+	//private CustomerDAO customerDAO;
+
+	public Model(ServletConfig config) throws ServletException {
+
+	}
+	
+}
