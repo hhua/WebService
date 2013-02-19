597dd6cd86779696b6c47336ced720c7babcb1db
Shoucong Chen <Shoucong Chen@ShoucongChen>
2013-02-19 16:09:29 -0500

    Add review information in detail search.

--- WebService/src/edu/cmu/ebiz/task8/bean/SearchPlaceDetailBean.java
+++ WebService/src/edu/cmu/ebiz/task8/bean/SearchPlaceDetailBean.java
@@ -19,87 +19,111 @@ public class SearchPlaceDetailBean implements Comparable<SearchPlaceDetailBean> 
 	private String reference;
 	private String website;
 	private int priceLevel;
-	private List<String> reviews;
+	private PlaceReviewBean[] reviews;
 	
 	public String getName() {
 		return name;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public double getLatitude() {
 		return latitude;
 	}
+
 	public double getLongitude() {
 		return longitude;
 	}
+
 	public double getRating() {
 		return rating;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public String getReference() {
 		return reference;
 	}
+
 	public String getWebsite() {
 		return website;
 	}
+
 	public int getPriceLevel() {
 		return priceLevel;
 	}
-	public List<String> getReviews() {
+
+	public PlaceReviewBean[] getReviews() {
 		return reviews;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
+
 	public void setLongitude(double longitude) {
 		this.longitude = longitude;
 	}
+
 	public void setRating(double rating) {
 		this.rating = rating;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public void setReference(String reference) {
 		this.reference = reference;
 	}
+
 	public void setWebsite(String website) {
 		this.website = website;
 	}
+
 	public void setPriceLevel(int priceLevel) {
 		this.priceLevel = priceLevel;
 	}
-	public void setReviews(List<String> reviews) {
+
+	public void setReviews(PlaceReviewBean[] reviews) {
 		this.reviews = reviews;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "SearchPlaceDetailBean [longitude=" + longitude + ", latitude=" + latitude
 				+ ", address=" + address + ", url=" + url + ", name=" + name + ", phone=" + phone + ", website=" + website
 				+ ", priceLevel=" + priceLevel + ", rating=" + rating
-				+ ", types=" + type + ", reference=" + reference + "]";
+				+ ", types=" + type + ", reference=" + reference
+				+ ", reviews=" + reviews[0].toString() + "]";
 	}
 	
 	@Override
