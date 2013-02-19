3034688b626c62fb9931c8d64b47b875a6da3f98
chinesecold <chinesecold@gmail.com>
2013-02-19 16:40:16 -0500

    Google API key updated

--- WebService/src/edu/cmu/ebiz/task8/basic/Constants.java
+++ WebService/src/edu/cmu/ebiz/task8/basic/Constants.java
@@ -1,6 +1,11 @@
 package edu.cmu.ebiz.task8.basic;
 
 public interface Constants {
-	public static String GOOGLE_API_KEY = "AIzaSyDy-3-hP8uDctn2XDeXw5EbV_H2Sza9WZg";			
+				
 	public static String BACKUP_KEY = "AIzaSyCVMKMZJQibQk8tl4kMGf23-jgDdMSpP4o";
+	public static String BACKUP_KEY2 = "AIzaSyDy-3-hP8uDctn2XDeXw5EbV_H2Sza9WZg";
+	public static String BACKUP_KEY3 = "";
+	public static String BACKUP_KEY4 = "";
+	public static String BACKUP_KEY5 = "";
+	public static String GOOGLE_API_KEY = BACKUP_KEY;
 }
